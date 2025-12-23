86772603c2291e5ddc28b53c18add4e7eb06abf6
fabian novoa <fabian93novoa@gmail.com>
2025-12-22 20:31:38 -0500

    creacion de dtos de user  y profiler

---
+++ ApiEcommerce/Models/Dtos/UserRegisterDto.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ApiEcommerce.Models.Dtos;
+
+public class UserRegisterDto
+{
+    
+    public string? ID { get; set; }
+    public required string Name { get; set; }
+    public required string Username { get; set; }
+    public string? Password { get; set; }
+    public string? Role { get; set; }
+
+}
